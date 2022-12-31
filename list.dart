e6d708daf4d5ca7d47a755c8987d72c9666c87e3
s.m.israt <smisratara25@gmail.com>
2022-12-31 13:00:41 +0600

    List basic 1st commit

---
+++ list.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  var name = ['Nowrin', 'Israt', 'Ara', '25'];
+  print(name);
+  var firstItem = name.first;
+  var lastItem = name.last;
+  var reverse = name.reversed;
+  var len = name.length;
+
+  print(firstItem);
+  print(lastItem);
+  print(reverse);
+  print(len);
+  ;
+}
