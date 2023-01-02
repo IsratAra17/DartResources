18b9fafc18206bb817fb4cf3c88363a033432de7
s.m.israt <smisratara25@gmail.com>
2023-01-02 09:50:08 +0600

    add some new code

--- FixedList.dart
+++ FixedList.dart
@@ -6,4 +6,9 @@ void main() {
   list[5] = 25;
   print(list);
   print(list.length);
+
+//this lines won't be executed in a fixed list.We can not add anything more in a fixed length list.
+  list.add(4);
+  print(list);
+  print(list.length);
 }
