dfdd212719b904316db511950ee199e65a895247
s.m.israt <smisratara25@gmail.com>
2023-01-03 09:00:02 +0600

    4 insertion methods in Dart[add(),addAll(),Insert(),insertAll() ]code

---
+++ insert.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  var mylist = [2, 25, 4, 6];
+  print("The list I have: \n");
+  print(mylist);
+  print("\n");
+
+  //syntax
+  //mylist.insert(index, element)
+
+  mylist.insert(0, 100);
+  print("After insert 1 element: \n");
+  print(mylist);
+  print("\n");
+
+//syntax for insertAll
+  //mylist.insertAll(index, iterable)
+
+  mylist.insertAll(2, [0, 0, 4, 5]);
+  print("After insertAll: \n");
+  print(mylist);
+  print("\n");
+
+  mylist.addAll([1000, 2000, 3000]);
+  print("After add iterate value");
+  print(mylist);
+
+  print("\n");
+
+  mylist.add(800);
+  print(mylist);
+}
